464efca53fa179f1ee3b8308fac240165b2293ca
akuntesmiage <115789043+akuntesmiage@users.noreply.github.com>
2024-12-08 06:33:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,90 @@
-import { join, dirname } from 'path'
-import { createRequire } from 'module'
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts'
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-import chalk from 'chalk'
-import { promises as fsPromises } from 'fs'
+import { join, dirname } from 'path';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { watchFile, unwatchFile } from 'fs';
+import cfonts from 'cfonts';
+import { createInterface } from 'readline';
+import yargs from 'yargs';
+import chalk from 'chalk';
+import { promises as fsPromises } from 'fs';
 
-let __dirname = dirname(fileURLToPath(import.meta.url))
-let require = createRequire(__dirname)
-let { say } = cfonts
-let rl = createInterface(process.stdin, process.stdout)
+const require = createRequire(import.meta.url);
+const cluster = require('cluster');
+const { setupMaster, fork } = cluster;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const rl = createInterface(process.stdin, process.stdout);
+
+const { say } = cfonts;
 
 say('Ai Hoshino', {
   font: 'chrome',
   align: 'center',
-  gradient: ['red', 'magenta']
-})
+  gradient: ['red', 'magenta'],
+});
 
 say(`Proximamente SenkoBot...`, {
   font: 'console',
   align: 'center',
-  gradient: ['red', 'magenta']
-})
+  gradient: ['red', 'magenta'],
+});
 
-var isRunning = false
+let isRunning = false;
 
 async function start(files) {
-  if (isRunning) return
-  isRunning = true
-  
+  if (isRunning) return;
+  isRunning = true;
+
   for (const file of files) {
-    const currentFilePath = new URL(import.meta.url).pathname
-    let args = [join(__dirname, file), ...process.argv.slice(2)]
+    const args = [join(__dirname, file), ...process.argv.slice(2)];
+
     say([process.argv[0], ...args].join(' '), {
       font: 'console',
       align: 'center',
-      gradient: ['red', 'magenta']
-    })
-    
+      gradient: ['red', 'magenta'],
+    });
+
     setupMaster({
       exec: args[0],
       args: args.slice(1),
-    })
-    
-    let p = fork()
-    p.on('message', data => {
-      console.log('[RECEIVED]', data)
+    });
+
+    let p = fork();
+
+    p.on('message', (data) => {
+      console.log('[RECEIVED]', data);
       switch (data) {
         case 'reset':
-          p.process.kill()
-          isRunning = false
-          start(files)
-          break
+          p.process.kill();
+          isRunning = false;
+          start(files);
+          break;
         case 'uptime':
-          p.send(process.uptime())
-          break
+          p.send(process.uptime());
+          break;
       }
-    })
-    
-    p.on('exit', (_, code) => {
-      isRunning = false
-      console.error('Ocurrió un error inesperado:', code)
-      start(files)
+    });
 
-      if (code === 0) return
+    p.on('exit', (_, code) => {
+      isRunning = false;
+      console.error('Ocurrió un error inesperado:', code);
+      start(files);
+
+      if (code === 0) return;
       watchFile(args[0], () => {
-        unwatchFile(args[0])
-        start(files)
-      })
-    })
-    
-    let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+        unwatchFile(args[0]);
+        start(files);
+      });
+    });
+
+    let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
     if (!opts['test'])
-      if (!rl.listenerCount()) rl.on('line', line => {
-        p.emit('message', line.trim())
-      })
+      if (!rl.listenerCount()) {
+        rl.on('line', (line) => {
+          p.emit('message', line.trim());
+        });
+      }
   }
 }
 
-start(['index.js'])
+start(['index.js']);
